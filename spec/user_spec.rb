24125f78d445f02dacb38bd66027d18d0180065f
NoelHsiao <77479612+huihsinhsiao@users.noreply.github.com>
2021-04-01 01:27:12 +0800

    Update user_spec.rb

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,21 @@
-require 'user'
+# 收費規則
+# 會員：
+# 非會員（單次租車）：
+
+require '../lib/user'
 
 RSpec.describe User do
-  pending '試著動手寫一些測試吧！'
+  context "區分會員" do
+    it "是會員" do
+      user = User.new
+      result = user.mobile
+      expect(result).to be true
+    end
+
+    it "非會員" do
+      user = User.new
+      result = user.mobile
+      expect(result).to be false
+    end
+  end
 end
